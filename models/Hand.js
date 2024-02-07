06e533fd3368d9287898331dce9ba7a407f6fb33
jdub <jessicaclaing@gmail.com>
2024-02-07 14:54:43 -0800

    added card and hand models

---
+++ models/Hand.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Hand extends Model {}
+
+Hand.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+        },
+        character_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Character',
+                key: 'id',
+            }
+        },
+        card_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Card',
+                key: 'id',
+            }
+        }
+    }
+)
