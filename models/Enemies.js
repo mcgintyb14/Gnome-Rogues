9294b44eee5cc94f5df5205c4a1a4f913294084e
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-02-08 13:50:12 -0500

    added server.js file and Enemy model

---
+++ models/Enemies.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Enemies extends Model {}
+
+Enemies.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        agility: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        hp: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        special_move: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'enemies',
+    }
+);
+
+module.exports = Enemies;
