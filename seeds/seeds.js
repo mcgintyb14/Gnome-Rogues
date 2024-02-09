75dc87194208b5976a6d177544b71ad432ba64e5
jdub <jessicaclaing@gmail.com>
2024-02-08 20:33:09 -0800

    fixed a few connection bugs: 1. added in util folder for the server hbs/helper. 2. updated seeds.js function. commented out the individual files, will uncomment on next com mit. 3. fixed commas in enemies.json. 4. updated config/connection file - used verbage to make it compatible with b oth windows and mac

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,50 +1,57 @@
 const sequelize = require('../config/connection');
-const gnomeData = require('./Gnome.json');
-const cardData = require('./Card.json');
-const enemiesData = require('./Enemies.json');
-const userData = require('./User.json');
-const handData = require('./Hand,json');
-const deckData = require('./Deck.json');
-const characterData = require('./character.json');
-const { User, Character, Enemies, Gnome, Card, Hand, Deck } = require('../models');
+// const gnomeData = require('./Gnome.json');
+// const cardData = require('./Card.json');
+// const enemiesData = require('./Enemies.json');
+// const userData = require('./User.json');
+// const handData = require('./Hand,json');
+// const deckData = require('./Deck.json');
+// const characterData = require('./character.json');
+// const { User, Character, Enemies, Gnome, Card, Hand, Deck } = require('../models');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-     returning: true
-  });
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+    await sequelize.sync({ force: true });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
-  await Gnome.bulkCreate(gnomeData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await User.bulkCreate(userData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
+  //
+  // await Gnome.bulkCreate(gnomeData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await Card.bulkCreate(cardData, {
-    individualHooks: true,
-     returning: true
-  });
+  // await Card.bulkCreate(cardData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
 
-  await Enemies.bulkCreate(enemiesData, {
-    individualHooks: true,
-     returning: true
-  });
+  // await Enemies.bulkCreate(enemiesData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
+  //
+  // await Character.bulkCreate(characterData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
 
-  await Character.bulkCreate(characterData, {
-    individualHooks: true,
-     returning: true
-  });
-
-  await Deck.bulkCreate(deckData, {
-    individualHooks: true,
-     returning: true
-  });
-
-  await Hand.bulkCreate(handData, {
-    individualHooks: true,
-     returning: true
-  });
+  // await Deck.bulkCreate(deckData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
+  //
+  // await Hand.bulkCreate(handData, {
+  //   individualHooks: true,
+  //    returning: true
+  // });
 
   process.exit(0);
 };
