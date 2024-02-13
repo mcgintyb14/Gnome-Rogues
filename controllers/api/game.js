15e9897c93b1268459f500bba92400ebe3f208ac
jdub <jessicaclaing@gmail.com>
2024-02-13 01:29:39 -0800

    created attack route

--- controllers/api/game.js
+++ controllers/api/game.js
@@ -1,39 +1,16 @@
 const router = require('express').Router();
-const { getCards } = require('../../utils/helpers');
+const { getCards } = require('../../utils');
 const { User, Character, Enemies, Gnome, Card, Hand, Deck, SavedGame } = require('../../models');
-// const { generateUniqueId } = require('../../utils/uniequeid');
 const multer = require('multer');
 const upload = multer();
 
 let randomCards;
 let deck = [];
 
-// router.get('/:id', async (req, res) => {
-//     try {
-//         debugger
-//         const character = await Character.findByPk(req.query.class_id);
-//         const classData = await Gnome.findByPk(character.class_id);
-//         const allEnemies = await Enemies.findByPk();
-
-    
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
 router.get('/loading', async (req, res) => {
     try {
         const { class_id } = req.query;
-
-        // const characterId = generateUniqueId(); 
-        // req.session.characterId = characterId;
-
         const gnomeData = await Gnome.findByPk(class_id);
-
-        // const handData = await Hand.findByPk(req.session.characterId);
-        // const characterData = await Character.findByPk(req.session.characterId);
-        
         res.render('loading', { Gnome: gnomeData });
     } catch (err) {
         console.error(err);
@@ -43,67 +20,39 @@ router.get('/loading', async (req, res) => {
 
 router.post('/submit-character-name/:class_id', upload.none(), async (req, res) => {
     try {
-        // const { characterName } = req.body;
-        // const characterId = req.session.characterId; // Retrieve character ID from session
-        // Find the existing character by ID and update its name
-        // const updatedCharacter = await Character.findByPk(characterId);
         const createdCharacter = await Character.create({
             name: req.body.name,
             class_id: req.params.class_id,
             current_hp: req.body.current_hp
         });
-        const character = createdCharacter.get({plain: true})
-        // const newChar = character.findByPk(character.id, {include: [{model: Character}]});
-        // const charData = character.get({plain: true});
-        const classData = await Gnome.findByPk(character.class_id); 
+
+        const characterData = createdCharacter.get({plain: true});
+        const classData = await Gnome.findByPk(characterData.class_id); 
         const allEnemies = await Enemies.findAll();
         const randomIndex = Math.floor(Math.random() * allEnemies.length);
-        const randomEnemy = allEnemies[randomIndex];
+        const enemyData = allEnemies[randomIndex];
         
-        const game = await SavedGame.create({
-            character_id: character.id,
-            character_name: character.name,
-            class_id: character.class_id,
+        const savedGame = await SavedGame.create({
+            character_id: characterData.id,
+            character_name: characterData.name,
+            class_id: characterData.class_id,
             character_current_hp: classData.MaxHP,
             enemy_id: randomIndex,
-            enemy_current_hp: randomEnemy.hp
+            enemy_current_hp: enemyData.hp
           });
-        if (game) {
-            // updatedCharacter.name = characterName;
-            // await updatedCharacter.save(); // Save the updated character
-            // res.render('game', { character });
-            res.status(200).send({ gameId: game.id })
+    
+        if (savedGame) {
+            const gameData = savedGame.get({ plain: true });
+            // res.status(200).send(plain)
+            res.status(200).send({ gameId: gameData.id })
+            // res.render('game', { game: gameData, character: characterData, enemy: enemyData, class: classData  })
         } else {
             res.status(404).json({ error: 'Character not found' });
         }
     } catch (err) {
-        console.error(err);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
-// router.get('/loading-character', async (req, res) => {
-//     try {
-//         debugger
-//         console.log("landed in this route");
-//         const class_id = req.query.class_id;
-//         // Fetch the newly posted characterName data from the session or wherever it's stored
-//         // const characterName = req.session.characterName; // Adjust this according to how you're storing the characterName
-        
-//         // Fetch the same data as the /loading route
-//         const gnomeData = await Gnome.findByPk(class_id);
-//         const handData = await Hand.findByPk(req.session.characterId);
-//         const characterData = await Character.findByPk(req.session.characterId);
-//         console.log(characterName); // Log characterName to the console
-        
-//         // Render the "loading-character" handlebars file and pass the fetched data
-//         res.render('loading-character', { cards: handData, character: characterData, Gnome: gnomeData, characterName: characterName });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
 
 //route to handle frontend request for a new set of 4 cards to generate and sends to handlebars file
 router.get('/getcards', async (req, res) => {
@@ -117,12 +66,21 @@ router.get('/getcards', async (req, res) => {
     }
 });
 
-router.post('/selectCard/', (req, res) => {
-    console.log(req.body);
+router.post('/:id/attack', async (req, res) => {
+    const card = req.body;
+    const gameId = req.params.id;
+    const model = await SavedGame.findByPk(gameId);
+    const game = await model.get({ plain: true });
+    const remainingHealth = game.enemy_current_hp - card.damage;
+    model.enemy_current_hp = remainingHealth;
+    await model.save();
+    const gameAgain = await model.get({ plain: true });
 
-})
-//sending data to frontend
-//         res.json({ enemy: randomEnemy, character: characterData, gnomeClass: classData });
+    if (remainingHealth <= 0) {
+        res.status(200).send('You have Slayed the gnenemy!')
+    }
+    res.status(200).send('ok')
+});
 
 router.get('/test-error', (req, res, next) => {
     try {
